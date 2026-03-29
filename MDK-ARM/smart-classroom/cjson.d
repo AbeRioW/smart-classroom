13f06258839733ac6958b59d7dff34a7c7dd7ecd
AbeRioW <261507455@qq.com>
2026-03-29 16:32:12 +0800

    cjson added

---
+++ MDK-ARM/smart-classroom/cjson.d
@@ -0,0 +1,10 @@
+smart-classroom\cjson.o: ..\App\cJSON.c
+smart-classroom\cjson.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+smart-classroom\cjson.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+smart-classroom\cjson.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+smart-classroom\cjson.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+smart-classroom\cjson.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\limits.h
+smart-classroom\cjson.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\ctype.h
+smart-classroom\cjson.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\float.h
+smart-classroom\cjson.o: ..\App\cJSON.h
+smart-classroom\cjson.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
